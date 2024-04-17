716e3d53d5fde2b8a49b96731f7a7e8d04b7dd27
Kleiton MQ <kleiton.x5@gmail.com>
2024-04-16 21:31:25 -0300

    configuracao de dependencias do projeto, elaboracao da pagina index com formulario

---
+++ src/main/java/com/unopar/CadUsuario/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.unopar.CadUsuario.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    
+    @RequestMapping("/")
+    public String home(){
+        return "index";
+    }
+}
