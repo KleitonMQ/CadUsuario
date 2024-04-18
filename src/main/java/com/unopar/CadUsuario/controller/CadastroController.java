d92518a4e77264ba377c76eea05474b0f710b054
Kleiton MQ <kleiton.x5@gmail.com>
2024-04-18 11:46:19 -0300

    criada controller para cadastro, removido dependencia do service na classe homecontroller, editado html para enviar as informacoes e informar os campos obrigatorios.

---
+++ src/main/java/com/unopar/CadUsuario/controller/CadastroController.java
@@ -0,0 +1,24 @@
+package com.unopar.CadUsuario.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.unopar.CadUsuario.model.Cadastro;
+import com.unopar.CadUsuario.service.CadastroService;
+
+@Controller
+@RequestMapping("/cadastro")
+public class CadastroController {
+    
+    @Autowired
+    private CadastroService cadastroService;
+
+    @PostMapping("/salvar")
+    public String salvarCadastro(@RequestParam("nome") String nome, ){
+        Cadastro cadastro = new Cadastro();
+        return "redirect:/sucesso";
+    }
+}
