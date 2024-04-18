52280d90204d9ca8f1da2f92ea35484268af04d1
Kleiton MQ <kleiton.x5@gmail.com>
2024-04-18 17:17:52 -0300

    comit antes do merge

--- src/main/java/com/unopar/CadUsuario/controller/HomeController.java
+++ src/main/java/com/unopar/CadUsuario/controller/HomeController.java
@@ -2,7 +2,6 @@ package com.unopar.CadUsuario.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.unopar.CadUsuario.service.CadastroService;
